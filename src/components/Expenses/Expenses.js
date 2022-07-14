849f3d8d55fd6ceae6d505cc3c5c86d8cbe74bf4
John West <contactjohnwest@gmail.com>
2022-07-13 21:26:56 -0700

    Upload complete expense tracker

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear() === parseInt(filteredYear)
+  );
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
